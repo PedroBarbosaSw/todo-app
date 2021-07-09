fba818c56f265c900556b2c8b6a8b827923a3cf7
Pedro Barbosa <pedro_dith@hotmail.com>
2021-07-09 15:17:41 -0300

    config express server

---
+++ backend/src/config/server.js
@@ -0,0 +1,13 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+//urlEncoded é o padrão usado para submissao de formulários.
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function() {
+   console.log(`Backend is running on port ${port}.`)
+})
