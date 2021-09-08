3fc5fcab4824a7559d0299a585fc3531bfeb4f06
Pedro Barbosa <pedro_dith@hotmail.com>
2021-09-08 15:58:17 -0300

    commit test2

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -5,7 +5,7 @@ export default props => (
    <div>
       <PageHeader name='Sobre' small='Nós' />
 
-      <h2>Nossa História</h2>
+      <h2>Nossa História:</h2>
       <p>Lorem ipsum...</p>
       <h2>Missão + Visão</h2>
    </div>
