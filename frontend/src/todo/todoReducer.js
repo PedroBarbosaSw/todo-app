70a7ea32ad71bc4afd375c2699799b69aab012d5
Pedro Barbosa <pedro_dith@hotmail.com>
2021-11-11 22:08:47 -0300

    apply middleware

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,19 +1,4 @@
-const INITIAL_STATE = {
-      description: 'Ler livro',
-      list: [{
-         _id: 1,
-         description: 'Pagar fatura do cartao',
-         done: true
-      }, {
-         _id: 2,
-         description: 'Reuniao com a equipe as 10:00',
-         done: false
-      }, {
-         _id: 3,
-         description: 'Consulta medica na terça depois do almoço',
-         done: false
-      }]
-}
+const INITIAL_STATE = { description: '', list: [] }
 
 export default (state = INITIAL_STATE, action) => {
    switch(action.type) {
